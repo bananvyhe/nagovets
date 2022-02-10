9e39a5c841d01e93c9f732dd171ee64643b82291
bananvyhe <loadonden@yahoo.com>
2022-02-10 22:18:39 +0500

    Set Vue properties

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,7 +1,17 @@
 const { environment } = require('@rails/webpacker')
 const { VueLoaderPlugin } = require('vue-loader')
 const vue = require('./loaders/vue')
+const { DefinePlugin } = require('webpack')
 
 environment.plugins.prepend('VueLoaderPlugin', new VueLoaderPlugin())
 environment.loaders.prepend('vue', vue)
 module.exports = environment
+
+environment.plugins.prepend(
+    'Define',
+    new DefinePlugin({
+        __VUE_OPTIONS_API__: false,
+        // or __VUE_OPTIONS_API__: true,
+        __VUE_PROD_DEVTOOLS__: false
+    })
+)
